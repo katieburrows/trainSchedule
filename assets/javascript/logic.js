77fb81b06a9b4c90a072d374ad230f3eed58d3e0
katieburrows <katie_burrows@me.com>
2019-05-13 18:40:25 -0600

    got form wired up

---
+++ assets/javascript/logic.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    
+  var firebaseConfig = {
+    apiKey: "AIzaSyAB8eJ6wUyZZLOQ5YlaJd10HvB00fuzSnU",
+    authDomain: "trainschedule-2485c.firebaseapp.com",
+    databaseURL: "https://trainschedule-2485c.firebaseio.com",
+    projectId: "trainschedule-2485c",
+    storageBucket: "trainschedule-2485c.appspot.com",
+    messagingSenderId: "831721997299",
+    appId: "1:831721997299:web:56fb34f17e7c8993"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+//   var database = firebase.database();
+
+  $(".btn").on("click", function(event){
+      event.preventDefault();
+      
+      var trainName = $("#trainName").val().trim();
+      var destination = $("#destination").val().trim();
+      var firstTrain = $("#firstTrain").val().trim();
+      var frequency = $("#frequency").val().trim();
+
+      $("#trainName").val("");
+      $("#destination").val("");
+      $("#firstTrain").val("");
+      $("#frequency").val("");
+      
+      console.log(trainName, destination, firstTrain, frequency);
+
+  });
+
+
+
+
+
+
+}) 
