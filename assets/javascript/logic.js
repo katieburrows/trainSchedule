ad0a0fe44550610a8a9372401c7c7961841a3521
katieburrows <katie_burrows@me.com>
2019-05-14 20:50:58 -0600

    got firebase talking to form

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
     
-  var firebaseConfig = {
-    apiKey: "AIzaSyAB8eJ6wUyZZLOQ5YlaJd10HvB00fuzSnU",
-    authDomain: "trainschedule-2485c.firebaseapp.com",
-    databaseURL: "https://trainschedule-2485c.firebaseio.com",
-    projectId: "trainschedule-2485c",
-    storageBucket: "trainschedule-2485c.appspot.com",
-    messagingSenderId: "831721997299",
-    appId: "1:831721997299:web:56fb34f17e7c8993"
-  };
+    var firebaseConfig = {
+        apiKey: "AIzaSyAB8eJ6wUyZZLOQ5YlaJd10HvB00fuzSnU",
+        authDomain: "trainschedule-2485c.firebaseapp.com",
+        databaseURL: "https://trainschedule-2485c.firebaseio.com",
+        projectId: "trainschedule-2485c",
+        storageBucket: "trainschedule-2485c.appspot.com",
+        messagingSenderId: "831721997299",
+        appId: "1:831721997299:web:56fb34f17e7c8993"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-//   var database = firebase.database();
+    var database = firebase.database();
 
   $(".btn").on("click", function(event){
       event.preventDefault();
@@ -27,12 +26,32 @@ $(document).ready(function() {
       $("#destination").val("");
       $("#firstTrain").val("");
       $("#frequency").val("");
-      
-      console.log(trainName, destination, firstTrain, frequency);
+
+      database.ref().push({
+          trainName: trainName,
+          destination: destination,
+          firstTrain: firstTrain,
+          frequency: frequency
+      });
 
   });
 
+  database.ref().on("child_added", function(snapshot){
+    var sv = snapshot.val();
+    var trainName = $("<td>").text(sv.trainName);
+    var destination = $("<td>").text(sv.destination);
+    var firstTrain = $("<td>").text(sv.firstTrain);
+    var frequency = $("<td>").text(sv.frequency);
 
+    // var newTr = "";
+
+    // newTr.append(trainName, destination, firstTrain, frequency);
+
+    // $("tbody").append(newTr);
+
+  }, function (errorObject){
+      console.log("Errors handled: " + errorObject);
+  });
 
 
 
